aa1fa694f51f73e61f2085c2f8a4866053dbf67e
Daniel-Szego <szego.daniel@gmail.com>
2018-10-08 13:21:33 +0200

    init and clear function

--- script/script.js
+++ script/script.js
@@ -1,12 +1,12 @@
 /**
- * Transaction file for green supply chain
+ * Transaction file for food supply chain
  */
 
-const namespace = "org.supplychain.green.model";
+const namespace = "org.supplychain.food.model";
 
 /**
  *
- * @param {org.supplychain.green.model.InitTestData} param - model instance
+ * @param {org.supplychain.food.model.InitTestData} param - model instance
  * @transaction
  */
 async function InitTestDataFunction(param) {  
@@ -16,164 +16,142 @@ async function InitTestDataFunction(param) {
 
     console.log('init test data');
 
-    console.log('Creating a manufacturer company');  
+    console.log('Creating a Production state');  
     const factory = getFactory(); 
 	
-  	// adding manufacturer company 
-    const manCompReg = await getParticipantRegistry(namespace + '.ManufacturerCompany');   
-    const manComp = await factory.newResource(namespace, 'ManufacturerCompany', "1");
-    manComp.companyName = "Cell phone Manufacturer Inc.";
-    manComp.GHG = baseline;
-    const newAddress = await factory.newConcept(namespace, 'CompanyAddress');
+  	// adding Production state 
+    const productionReg = await getParticipantRegistry(namespace + '.Production');   
+    const production = await factory.newResource(namespace, 'Production', "1");
+    production.stateName = "Cell phone Manufacturer Inc.";
+    production.GHG = baseline;
+    const newAddress = await factory.newConcept(namespace, 'Address');
 	newAddress.country = "Bejing";
 	newAddress.city = "China";
 	newAddress.street = "Xia Mo Street";
     newAddress.hauseNr = 16;
-  	manComp.companyAddress = newAddress;
+  	production.stateAddress = newAddress;
   
-    await manCompReg.add(manComp);       
+    await productionReg.add(production);       
 
-  	// adding transportation company 1
-    console.log('Creating a transportation company 1');  
+  	// adding Processing State
+    console.log('Creating a Processing state');  
 
-    const transCompReg = await getParticipantRegistry(namespace + '.TransportationCompany');   
-    const transComp1 = await factory.newResource(namespace, 'TransportationCompany', "2");
-    transComp1.companyName = "Truck Transport Inc.";
-    transComp1.GHG = baseline / 2;
-    const newAddress2 = await factory.newConcept(namespace, 'CompanyAddress');
+    const processingReg = await getParticipantRegistry(namespace + '.Processing');   
+    const processing = await factory.newResource(namespace, 'Processing', "2");
+    processing.stateName = "Truck Transport Inc.";
+    processing.GHG = baseline / 2;
+    const newAddress2 = await factory.newConcept(namespace, 'Address');
 	newAddress2.country = "Hong Kong";
 	newAddress2.city = "China";
 	newAddress2.street = "Mua Mo Street";
     newAddress2.hauseNr = 22;
-  	transComp1.companyAddress = newAddress2;
-    transComp1.transportFrom = new Array();
-    transComp1.transportFrom.push(manComp);
+  	processing.stateAddress = newAddress2;
+    processing.stateFrom = new Array();
+    processing.stateFrom.push(production);
   
-    await transCompReg.add(transComp1);       
+    await processingReg.add(processing);       
   
-    // adding relay company 1
-    console.log('Creating a relay company 1');  
+    // adding Distribution State 
+    console.log('Creating a Distribution State');  
 
-    const relayCompReg = await getParticipantRegistry(namespace + '.RelayCompany');   
-    const relayComp1 = await factory.newResource(namespace, 'RelayCompany', "5");
-    relayComp1.companyName = "Harbour";
-    relayComp1.GHG = baseline / 1.75;
-    const newAddress5 = await factory.newConcept(namespace, 'CompanyAddress');
+    const distributionReg = await getParticipantRegistry(namespace + '.Distribution');   
+    const distribution = await factory.newResource(namespace, 'Distribution', "3");
+    distribution.stateName = "Harbour";
+    distribution.GHG = baseline / 1.75;
+    const newAddress5 = await factory.newConcept(namespace, 'Address');
 	newAddress5.country = "China";
 	newAddress5.city = "Hong Kong";
 	newAddress5.street = "Seeside";
     newAddress5.hauseNr = 4;
-  	relayComp1.companyAddress = newAddress5;
+  	distribution.stateAddress = newAddress5;
+    distribution.stateFrom = new Array();
 
-    await relayCompReg.add(relayComp1);       
-
+    await distributionReg.add(distribution);       
   
-  	// adding transportation company 2
-    console.log('Creating a transportation company 2');  
-
-    const transComp2 = await factory.newResource(namespace, 'TransportationCompany', "3");
-    transComp2.companyName = "Shipping Inc.";
-    transComp2.GHG = baseline * 1.5;
-    const newAddress3 = await factory.newConcept(namespace, 'CompanyAddress');
+  	// adding Retail State
+    console.log('Creating a Retail State');  
+  
+    const retailReg = await getParticipantRegistry(namespace + '.Retail'); 
+    const retail = await factory.newResource(namespace, 'Retail', "4");
+    retail.stateName = "Shipping Inc.";
+    retail.GHG = baseline * 1.5;
+    const newAddress3 = await factory.newConcept(namespace, 'Address');
 	newAddress3.country = "Sydney";
 	newAddress3.city = "Australia";
 	newAddress3.street = "Beecon Str";
     newAddress3.hauseNr = 122;
-  	transComp2.companyAddress = newAddress3;
-    transComp2.transportFrom = new Array();
-    transComp2.transportFrom.push(relayComp1);  
+  	retail.stateAddress = newAddress3;
+    retail.stateFrom = new Array();
+    retail.stateFrom.push(distribution);  
   
-    await transCompReg.add(transComp2);       
+    await retailReg.add(retail);       
 
-    // adding relay company 2
-    console.log('Creating a relay company 2');  
+    // adding Restaurant 
+    console.log('Creating a Restaurant State');  
 
-    const relayComp2 = await factory.newResource(namespace, 'RelayCompany', "6");
-    relayComp2.companyName = "Harbour";
-    relayComp2.GHG = baseline * 1.25;
-    const newAddress6 = await factory.newConcept(namespace, 'CompanyAddress');
+    const restaurantReg = await getParticipantRegistry(namespace + '.Restaurant'); 
+    const restaurant = await factory.newResource(namespace, 'Restaurant', "5");
+    restaurant.stateName = "Harbour";
+    restaurant.GHG = baseline * 1.25;
+    const newAddress6 = await factory.newConcept(namespace, 'Address');
 	newAddress6.country = "Italy";
 	newAddress6.city = "Triest";
 	newAddress6.street = "Via Rue";
     newAddress6.hauseNr = 44;
-  	relayComp2.companyAddress = newAddress6;
-
-    await relayCompReg.add(relayComp2);       
-
+  	restaurant.stateAddress = newAddress6;
+    restaurant.stateFrom = new Array();
+    restaurant.stateFrom.push(retail);  
   
-    // adding transportation company 3
-    console.log('Creating a transportation company 3');  
-
-    const transComp3 = await factory.newResource(namespace, 'TransportationCompany', "4");
-    transComp3.companyName = "Fast Train Transport";
-    transComp3.GHG = baseline * 0.8;
-    const newAddress4 = await factory.newConcept(namespace, 'CompanyAddress');
-	newAddress4.country = "Italy";
-	newAddress4.city = "Venice";
-	newAddress4.street = "Via Margutta";
-    newAddress4.hauseNr = 8;
-  	transComp3.companyAddress = newAddress4;
-    transComp3.transportFrom = new Array();
-    transComp3.transportFrom.push(relayComp2);
-
-    await transCompReg.add(transComp3);       
- 
-  	// adding sales company 
-    console.log('Creating a Sales company 1');  
-
-    const salesCompReg = await getParticipantRegistry(namespace + '.SalesCompany');   
-    const salesComp = await factory.newResource(namespace, 'SalesCompany', "5");
-    salesComp.companyName = "Sales Co.";
-    salesComp.GHG = baseline * 0.4;
-    const newAddress7 = await factory.newConcept(namespace, 'CompanyAddress');
-	newAddress7.country = "Hungary";
-	newAddress7.city = "Budapest";
-	newAddress7.street = "Bartok Bela ut";
-    newAddress7.hauseNr = 44;
-  	salesComp.companyAddress = newAddress7;
-  
-    await salesCompReg.add(salesComp);         
+    await restaurantReg.add(restaurant);       
 }
 
 /**
  *
- * @param {org.supplychain.green.model.ClearData} param - model instance
+ * @param {org.supplychain.food.model.ClearData} param - model instance
  * @transaction
  */
 async function ClearDataFunction(param) {  
     console.log('clearing test data');
 
     // deleting assets
-    const cellPhoneRegistry = await getAssetRegistry(namespace + '.CellPhone'); 
-    let cellPhones = await cellPhoneRegistry.getAll();
-    await cellPhoneRegistry.removeAll(cellPhones);
+    const CowReg = await getAssetRegistry(namespace + '.Cow'); 
+    let Cows = await CowReg.getAll();
+    await CowReg.removeAll(Cows);
+
+    const steakReg = await getAssetRegistry(namespace + '.Steak'); 
+    let steaks = await steakReg.getAll();
+    await steakReg.removeAll(steaks);
   
   	// deleting participants
-    const manCompRegistry = await getParticipantRegistry(namespace + '.ManufacturerCompany');
-    let manCompanies = await manCompRegistry.getAll();
-    await manCompRegistry.removeAll(manCompanies);
+    const productionReg = await getParticipantRegistry(namespace + '.Production');
+    let production = await productionReg.getAll();
+    await productionReg.removeAll(production);
     
-    const salesCompRegistry = await getParticipantRegistry(namespace + '.SalesCompany'); 
-    let salesCompanies = await salesCompRegistry.getAll();
-    await salesCompRegistry.removeAll(salesCompanies);
+    const processingReg = await getParticipantRegistry(namespace + '.Processing'); 
+    let processing = await processingReg.getAll();
+    await processingReg.removeAll(processing);
   
-    const relayCompRegistry = await getParticipantRegistry(namespace + '.RelayCompany'); 
-    let relayCompanies = await relayCompRegistry.getAll();
-    await relayCompRegistry.removeAll(relayCompanies);
+    const distributionReg = await getParticipantRegistry(namespace + '.Distribution'); 
+    let istribution = await distributionReg.getAll();
+    await distributionReg.removeAll(istribution);
 
-    const trCompRegistry = await getParticipantRegistry(namespace + '.TransportationCompany'); 
-    let trCompanies = await trCompRegistry.getAll();
-    await trCompRegistry.removeAll(trCompanies);
+    const retailReg  = await getParticipantRegistry(namespace + '.Retail'); 
+    let retail = await retailReg.getAll();
+    await retailReg.removeAll(retail);
+
+    const restaurantReg  = await getParticipantRegistry(namespace + '.Restaurant'); 
+    let restaurant = await restaurantReg.getAll();
+    await restaurantReg.removeAll(restaurant);
   
     console.log('clearing all data finished');  
 }
 
 /**
  *
- * @param {org.supplychain.green.model.Transfer} param - model instance
+ * @param {org.supplychain.food.model.Process} param - model instance
  * @transaction
  */
-async function TransferFunction(param) {  
+async function ProcessFunction(param) {  
 	let assetToTransfer = param.assetToTransfer;
     let fromCompany = param.fromCompany;
     let toCompany = param.toCompany;
@@ -210,7 +188,7 @@ async function TransferFunction(param) {
 
 /**
  *
- * @param {org.supplychain.green.model.Produce} param - model instance
+ * @param {org.supplychain.food.model.Produce} param - model instance
  * @transaction
  */
 async function ProduceFunction(param) {  
@@ -247,10 +225,10 @@ async function ProduceFunction(param) {
 
 /**
  *
- * @param {org.supplychain.green.model.Sell} param - model instance
+ * @param {org.supplychain.food.model.Consume} param - model instance
  * @transaction
  */
-async function SellFunction(param) {  
+async function ConsumeFunction(param) {  
 	let assetToTransfer = param.assetToSell;
     let factory = await getFactory();
  	
